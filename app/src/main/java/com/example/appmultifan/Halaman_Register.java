1b290f5832e05614dc196ea4e9f50e06bb886d7d
Yunita Yani <yunita278yani@gmail.com>
2022-01-24 20:40:29 +0700

    register

--- app/src/main/java/com/example/appmultifan/Halaman_Register.java
+++ app/src/main/java/com/example/appmultifan/Halaman_Register.java
@@ -2,7 +2,9 @@ package com.example.appmultifan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Halaman_Register extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Halaman_Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_halaman_register);
+    }
+
+    public void register(View view) {
+            Intent intent = new Intent(Halaman_Register.this, Halaman_Login.class);
+            startActivity(intent);
     }
 }
